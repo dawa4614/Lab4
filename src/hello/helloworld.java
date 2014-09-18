e779afc8932cd1b8019d52bc6cd452c1d64e6065
dawa4614 <dawa4614@colorado.edu>
2014-09-18 13:15:48 -0600

    Added string what's happening to hello world.java

--- src/hello/helloworld.java
+++ src/hello/helloworld.java
@@ -3,5 +3,6 @@ package hello;
 public class helloworld {
 	public static void main(String[] args){
 	System.out.println("hello world"); //display the string
+	System.out.println("What's happening?");
 	}
 }
